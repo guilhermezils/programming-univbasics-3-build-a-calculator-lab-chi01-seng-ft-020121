c7d582a87a599964587af15c0b9228ff39a678b7
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2020-12-22 00:29:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,42 @@
-def addition(num1, num2)
-
+def addition (num1, num2)
+  addition = #{num1} + #{num2}
+  puts addition
 end
 
-def subtraction(num1, num2)
+addition(5,4)
 
+def subtraction (num2, num1)
+  subtraction = #{num2} - #{num1}
+  puts subtraction
 end
 
-def division(num1, num2)
+subtraction (5,10)
 
+def division (num2, num1)
+  division = #{num2} / #{num1}
+  puts division
 end
 
-def multiplication(num1, num2)
+division(50,2)
 
+def multiplication (num1, num2)
+  multiplication = #{num1} * #{num2}
+  puts multiplication
 end
 
-def modulo(num1, num2)
+multiplication(4,30)
 
+def modulo (num1, num2)
+  modulo = #{num2} % #{num1}
+  puts modulo
 end
 
-def square_root(num)
+modulo(34,5)
 
+
+def square_root (num)
+  square_root = Math.sqrt(num)
+  puts square_root
 end
+
+square_root(81)
